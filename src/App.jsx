42e13774488bc747ca9c4b681c462fd6e1f4aa01
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-24 15:05:15 -0500

    usando server de GraphQL con fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
-import { useState } from 'react'
+
 import logo from './logo.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+ useEffect(() => {
+  fetch('http://localhost:4000',{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ query: `
+    query {
+      allPersons{
+        name
+      }
+    }
+    `})
+  })
+  .then(res => res.json())
+  .then(data => console.log(data.data))
+ })
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>GraphQL + React!</p>
       </header>
     </div>
   )
