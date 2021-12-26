72261b93facbb3617d87640a41200d296880b208
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-26 15:52:48 -0500

    configurando apollo client en nuestra app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { ApolloClient, HttpLink, gql} from '@apollo/client'
 
+const client = new ApolloClient({
+  link: new HttpLink ({
+    uri: 'http://localhost:4000'
+  })
+})
 ReactDOM.render(
   <React.StrictMode>
     <App />
