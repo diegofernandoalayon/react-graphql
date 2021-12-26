6eeae745d306d74b39d309f237c5e1323e8237e0
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-26 16:09:25 -0500

    agregando cache a la configuracion de apolloClient

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { ApolloClient, HttpLink, gql} from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink, gql} from '@apollo/client'
 
 const client = new ApolloClient({
+  cache: new InMemoryCache(),
   link: new HttpLink ({
     uri: 'http://localhost:4000'
   })
