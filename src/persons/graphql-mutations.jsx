ddba8f979be63f8443bc4ef56f2842a0677d8f00
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-29 20:25:39 -0500

    creando mutation para editar numero de telefono

--- src/persons/graphql-mutations.jsx
+++ src/persons/graphql-mutations.jsx
@@ -18,3 +18,17 @@ export const CREATE_PERSON = gql`
     }
   }
 `
+
+export const EDIT_NUMBER = gql`
+  mutation editNumber($name: String!, $phone: String!){
+    editNumber(name:$name, phone:$phone){
+      name
+      phone
+      address{
+        street
+        city
+      }
+      id
+    }
+  }
+`
